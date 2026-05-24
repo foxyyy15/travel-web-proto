3c7683f04ceea23eae85cca12a4fc2d7e8617ed1
Ardh <adiardian246@gmail.com>
2026-05-24 17:28:59 +0700

    .

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -55,6 +55,7 @@ export function Navbar() {
             width={160}
             height={40}
             className="h-10 w-auto object-contain"
+            style={{ width: 'auto' }}
             priority
           />
         </Link>
